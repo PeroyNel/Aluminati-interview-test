00a462fa2c19754c1c3a111e548bc7520352d911
Peroy Nel <peroynel@yahoo.com>
2022-03-17 11:19:39 +0200

    Passing the getLimit results as a property called number to the Number component
    Passing the getLimit results as a property called number to the Number component.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- In the following line of code I set parameters within which the input field can operate as stated by the specification sheet. Along with that I made sure to change the v-model to v-model.number, this ensures that the results that we get are returned as typeof(Number) === true.-->
     <input type="number" v-model.number="limit" min="1" max="100" />
-    <Numbers/>
+    <Numbers :number="getLimit"/>
   </div>
 </template>
 
