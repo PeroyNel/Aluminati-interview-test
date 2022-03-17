ccf62273f05e29173934d92b5d52024db2e7ffa3
Peroy Nel <peroynel@yahoo.com>
2022-03-17 10:19:59 +0200

    v-model change to v-model.number
    I made sure to change the v-model to v-model.number, this ensures that the results that we get returned is typeof(Number) === true.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    //In the following line of code I set parameters within which the input field can operate as stated by the specification sheet.
     <input type="number" v-model="limit" min="1" max="100" />
     <Numbers/>
   </div>
